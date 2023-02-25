66a12b37b9ecbe871859a9eb9afbbcc91dc52800
Muhammad Sameer <sameergb25@outlook.com>
2023-02-25 21:25:35 +0500

    implement countdown functionality

---
+++ Assets/Scripts/Controllers/Countdown/TimerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DungeonSurvivor.Core.Timer;
+using TMPro;
+using UnityEngine;
+
+namespace DungeonSurvivor.Controllers.Countdown
+{
+    public class TimerController : MonoBehaviour
+    {
+        [Header("CountDown parameters")] [SerializeField]
+        private int seconds = 180;
+
+        [SerializeField] private float     tickRate = 1f;
+        [SerializeField] private CountDown countDown;
+        [SerializeField] private TMP_Text  levelEndCountDownText;
+
+        private void ObserveTimer()
+        {
+            CountDownTime countDownTimer  = countDown.GetCountDownTime;
+            string        countDownString = countDownTimer.Minutes + " : " + countDownTimer.Seconds;
+            levelEndCountDownText.text = countDownString;
+
+            //TODO: Insert logic to broadcast progress
+        }
+
+        private void Start()
+        {
+            countDown.StartCountdown(seconds, tickRate);
+        }
+
+        private void Update()
+        {
+            ObserveTimer();
+        }
+    }
+}
