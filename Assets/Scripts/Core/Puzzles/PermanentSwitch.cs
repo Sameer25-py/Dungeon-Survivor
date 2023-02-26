805de0824040ef6d5231e5b3c212a42db98cea7e
Muhammad Sameer <sameergb25@outlook.com>
2023-02-26 16:06:39 +0500

    wire up gates and switches w/ animators

--- Assets/Scripts/Core/Puzzles/PermanentSwitch.cs
+++ Assets/Scripts/Core/Puzzles/PermanentSwitch.cs
@@ -1,3 +1,5 @@
+using System;
+using DungeonSurvivor.Controllers.Animations.Switch;
 using UnityEngine;
 using static DungeonSurvivor.Core.Events.GameplayEvents.Puzzles;
 
@@ -9,14 +11,28 @@ namespace DungeonSurvivor.Core.Puzzles
 
         [SerializeField] protected Gate _gate;
         
+        protected SwitchAnimator switchAnimator;
+
+        protected bool isSwitchPressed;
+        
         #endregion
 
         #region UnityLifeCycle
-        
+
+        private void OnEnable()
+        {
+            switchAnimator = GetComponent<SwitchAnimator>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
-            if (_gate) GateOpen?.Invoke(_gate.ID);
+            if (_gate && !isSwitchPressed)
+            {
+                isSwitchPressed = true;
+                switchAnimator.PressButton();
+                GateOpen?.Invoke(_gate.ID);
+            }
             else print("Gate reference not set");
         }
         
