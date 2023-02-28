7a1bb291b8cdf6be2e048259b9e00b58f6f47020
Muhammad Sameer <sameergb25@outlook.com>
2023-02-28 19:14:26 +0500

    add logic to track player moves

---
+++ Assets/Scripts/Analytics/Player/PlayerDataHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace DungeonSurvivor.Analytics.Player
+{
+    public static class PlayerDataHandler
+    {
+        public static int PlayerMoveCountPerStage = 0;
+        public static void ResetPlayerMoveCountPerStage()
+        {
+            PlayerMoveCountPerStage = 0;
+        }
+    }
+}
